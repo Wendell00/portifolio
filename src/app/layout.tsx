71eb39b80ddd7b560667c955eec99825cba4f884
wendell00 <wendellborges2003@gmail.com>
2023-11-06 19:48:14 -0300

    :sparkles: add: next-themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from './components/NavBar'
 import { FormContextProvider } from './context/FormContext'
+import { ThemeProvider } from 'next-themes'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -19,10 +20,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <FormContextProvider>
-        <NavBar/>
-          <div className='w-screen h-auto pt-[90px]'>
-            {children}
-          </div>
+          <NavBar/>
+          <ThemeProvider>
+            <div className='w-screen h-auto pt-[90px]'>
+              {children}
+            </div>
+          </ThemeProvider>
         </FormContextProvider>
         </body>
     </html>
