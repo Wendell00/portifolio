14dd0ccda55c89021a0e197e25945644f081c5b4
wendell00 <wendellborges2003@gmail.com>
2023-11-09 07:53:32 -0300

    fix: deploy error

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,19 +1,9 @@
-'use client'
-
-import React, { useState, useEffect } from "react";
-import { useTheme } from "next-themes";
-
 export default function Portfolio(){
-  const theme = useTheme();
-  const [background, setBackground] = useState(theme.background);
 
-  useEffect(() => {
-    document.body.style.backgroundColor = background;
-  }, [background]);
 
   return (
-    <div>
-      oi
-    </div>
+    <>
+      Portfolio
+    </>
   );
 };
