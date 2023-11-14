9539ae2e4c6354c826c12cb0861a3b2f0613094c
wendell00 <wendellborges2003@gmail.com>
2023-11-13 22:48:19 -0300

    configurando about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,19 +5,11 @@ import { motion } from 'framer-motion'
 export default function About() {
   return (
     <>
-        <div className="w-full h-[330vh]">
-              <motion.div
-                initial={{ y: -100, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ type: 'spring', stiffness: 30 }}>
-                  <h2 className={`title-about text-center text-[80px] mt-[24px]`}>Wendell Borges</h2>
-              </motion.div>
-              <motion.div
-                initial={{ y: -100, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ type: 'spring', stiffness: 30 }}>
-                  <p className={`text-2xl text-center text-[#b4bcd0]`}>Um pouco sobre mim</p>
-              </motion.div>
+        <div className="w-full h-auto">
+            <h2 className={`title-about text-center text-[70px] mt-[24px]`}>Wendell Borges</h2>
+            <div className="div">
+
+            </div>
         </div>
     </>
   )
