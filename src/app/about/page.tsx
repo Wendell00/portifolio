6aa39a0dfb314795c345ead592eb2d8c21e51aff
wendell00 <wendellborges2003@gmail.com>
2023-11-14 13:46:54 -0300

    :sparkles: feat: add frame-motion

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,10 +6,7 @@ export default function About() {
   return (
     <>
         <div className="w-full h-auto">
-            <h2 className={`title-about text-center text-[70px] mt-[24px]`}>Wendell Borges</h2>
-            <div className="div">
 
-            </div>
         </div>
     </>
   )
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,9 +1,29 @@
-export default function Portfolio(){
+'use client'
 
+import { motion } from 'framer-motion';
+import {useEffect, useState} from 'react'
+
+export default function Portfolio(){
+  const [scrollY, setScrollY] = useState(0);
+
+  const handleClick = () => {
+    setScrollY(window.scrollY)
+  }
 
   return (
     <>
-      <p className="white dark:text-red-900">Portfolio</p>
+      <div className="h-[330vh] w-screen" onClick={handleClick}>
+        <motion.div
+          style={{
+            width: 200,
+            height: 200,
+            backgroundColor: `rgb(${scrollY}, ${scrollY}, ${scrollY})`,
+            position: 'fixed',
+            marginTop: '18rem',
+          }}
+        >
+        </motion.div>
+      </div>
     </>
   );
 };
